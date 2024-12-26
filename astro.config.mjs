8e42cbb3ae887b7f261b6fa4f4d493ced8eebd93
Daniel Thorp <arcanistzed@gmail.com>
2024-12-26 14:24:39 -0500

    chore: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), astroI18next()],
+    integrations: [tailwind(), astroI18next(), icon()],
 });
