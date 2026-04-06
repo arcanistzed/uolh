9d83fff26fa18d7b82cff4c50a10cfcf92edca5b
Daniel Thorp <arcanistzed@gmail.com>
2026-04-06 09:21:36 -0400

    feat: add sitemap integration and robots.txt
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import astroI18next from "astro-i18next";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [astroI18next(), icon()],
+	site: "https://uodpa-apnuo.org",
+	integrations: [astroI18next(), icon(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
